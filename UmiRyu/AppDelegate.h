e9fbd24cd47f583c2b801c71289542f15b04eda5
jpboon <johan.boon@hva.nl>
2013-12-09 16:40:50 +0100

    Initial Commit

---
+++ UmiRyu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UmiRyu
+//
+//  Created by johan on 12/9/13.
+//  Copyright (c) 2013 Johan Boon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UmiRyu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UmiRyu
+//
+//  Created by johan on 12/9/13.
+//  Copyright (c) 2013 Johan Boon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
